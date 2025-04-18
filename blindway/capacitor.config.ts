6378f1efd20617644a83a0c5e48df50656a0105e
Diego <diegodiazlosada@gmail.com>
2025-04-18 17:56:58 +0200

    Instalación de la aplicación para IOS

--- blindway/capacitor.config.ts
+++ blindway/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ionic.blindway',
   appName: 'blindway',
-  webDir: 'www'
+  webDir: 'ww/browser'
 };
 
 export default config;
