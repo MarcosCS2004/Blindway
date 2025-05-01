3087ef39291f3472751a12038e4c35dee3b67685
MarcosCS2004 <128461258+MarcosCS2004@users.noreply.github.com>
2025-05-01 17:36:03 +0200

    Pagina de inicio y navegaciones

--- blindway/src/app/principal/principal.page.ts
+++ blindway/src/app/principal/principal.page.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons , IonMenuButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons , IonMenuButton, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
   styleUrls: ['./principal.page.scss'],
   standalone: true,
-  imports: [IonButtons,IonMenuButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonButtons,IonMenuButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToVisual(){
+    this.router.navigate(['/guia-visual']);
+  }
+
+  goToSonora(){
+    this.router.navigate(['/guia-sonora']);
   }
 
 }
