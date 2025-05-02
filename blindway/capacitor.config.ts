3aabf7e64f36c160d8594bbbc310b4d305d809f2
Diego <diegodiazlosada@gmail.com>
2025-05-02 10:59:38 +0200

    Arreglado la intalacion de android

--- blindway/capacitor.config.ts
+++ blindway/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ionic.blindway',
   appName: 'blindway',
-  webDir: 'ww/browser'
+  webDir: 'www'
 };
 
 export default config;
